efead8504dfea15a7dd60f8951c0386392f0a309
MayarSultanAlanaze <126346292+MayarSultanAlanaze@users.noreply.github.com>
2023-05-10 20:22:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,83 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        {
+            {
+                {
+                    {
+                        {
+                            {
+
+     } /* 1 question
+       for (int i= 1; i<= 100; i++) {
+             if ((i% 3==0)&&(i %5==0)) {
+                 System.out.println("FizzBuzz");
+             } else if ((i % 3) == 0){
+                System.out.println("Fizz");
+            }else if ((i%5)==0) {
+                System.out.println("Buzz"); */
+                        } /* 3 question
+       Scanner s=new Scanner(System.in);
+        System.out.printf("please enter the number");
+         int num1= s.nextInt();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num1+ "*" + (i+1)+"="+(num1*(i+1)));  */
+                    }/* 4
+        Scanner s=new Scanner(System.in);
+        int num;
+        int fact=1;
+        System.out.printf("please enter the number");
+        num= s.nextInt();
+
+        for (int i = 1; i <=num; i++) {
+           fact *=i;
+        }
+        System.out.println("factorial: " + fact); */
+                }/* 5
+        int n=10,a=8, result=1;
+        if (n>0&&a==0){
+            result=1;
+        } else if (n==0 && a>=1) {
+            result=1;
+        }else {
+            for (int i=1; i<=m;i++){
+                result=result*n;
+        }
+            System.out.println(result); */
+            } /* 6 question
+        int counter;
+        int num =0;
+        int sumeven =0;
+        int sumodd =0;
+
+
+        for ( int i = 1; i <=10;i++) {
+            System.out.println(i);
+            if (i %2==0){
+               sumeven = sumeven +i;
+            }else {
+                sumodd=sumodd+i;
+            }
+            System.out.println("Even sum"+sumeven);
+            System.out.println("Odd sum"+sumodd); */
+        } /* 7 question
+        Scanner S = new Scanner(System.in);
+        System.out.printf("please enter the positive integer");
+        int num=S.nextInt();
+        int i=1;
+       boolean flag=false;
+        while ( i <= num/1) {
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+            ++i;
+        }
+        if (flag)
+            System.out.println(num + " is a prime number");
+        else
+            System.out.println(num + " is not a prime number"); */
+
+    }
+}
